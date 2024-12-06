ff6a81e34e8ead632a7eb8cdaa7eaca782d0bb87
Entkenntnis <Entkenntnis@users.noreply.github.com>
2024-12-06 22:25:33 +0100

    moving on with worms

--- src/content/worms/worms-management.js
+++ src/content/worms/worms-management.js
@@ -6,14 +6,14 @@ import { renderPage } from '../../helper/render-page.js'
  */
 export function setupWormsManagement(App) {
   App.express.get('/worms/drafts', async (req, res) => {
-    if (!req.user) {
+    const user = req.user
+    if (!user) {
       res.redirect('/')
       return
     }
 
     const bots = await App.db.models.WormsBotDraft.findAll({
-      where: { UserId: req.user.id },
-      raw: true,
+      where: { UserId: user.id },
     })
 
     renderPage(App, req, res, {
@@ -21,9 +21,18 @@ export function setupWormsManagement(App) {
       heading: 'Deine Bots',
       backButton: false,
       content: `
-        <p>${JSON.stringify(bots)}</p>
+        ${bots
+          .map(
+            (bot) =>
+              `<div style="margin-bottom: 24px;">${
+                bot.name
+              }, zuletzt bearbeitet ${App.moment(bot.updatedAt).locale('de').fromNow()} [<a href="/worms/drafts/edit?id=${bot.id}">bearbeiten</a>] [<a href="/worms/drafts/delete?id=${
+                bot.id
+              }">löschen</a>]</div>`
+          )
+          .join('')}
 
-        <p>Neuen Bot erstellen</p>
+        <form action="/worms/drafts/create"><input name="name"> <input type="submit" value="Neuen Bot erstellen"></p>
       `,
     })
   })
@@ -45,12 +54,120 @@ export function setupWormsManagement(App) {
       name,
       UserId: req.user.id,
       code: `
-function think(dx, dy, board, dir, x, y, z) {
-  
+/**
+ * Bestimme bei jedem Schritt deines Bots die Laufrichtung
+ * 
+ * @param {number} dx         Breite des Spielfelds (fixiert auf 74)
+ * @param {number} dy         Höhe des Spielfelds (fixiert auf 42)
+ * @param {number[][]} board  Zwei-dimensionales Feld, board[x][y] beschreibt den Inhalt bei (x|y) mit
+ *                            -1 = Wand, 0 = frei, 1 = von dir besetzt, 2 = vom Gegner besetzt
+ * @param {number} x          x-Koordinate deines Bots
+ * @param {number} y          y-Koordinate deines Bots
+ * @param {number} dir        Laufrichtung deines Bots (0 = hoch, 1 = rechts, 2 = runter, 3 = links)
+ * @param {number} oppX       x-Koordinate des Gegners
+ * @param {number} oppY       y-Koordinate des Gegners
+ *
+ * @returns {number}          Neue Laufrichtung
+ */
+function think(dx, dy, board, x, y, dir, oppX, oppY) {
+  // Schreibe hier deinen Code
+
+  return dir
 }
 `,
     })
 
     res.redirect('/worms/drafts')
+  })
+
+  App.express.get('/worms/drafts/delete', async (req, res) => {
+    if (!req.user) {
+      res.redirect('/')
+      return
+    }
+
+    const id = req.query.id ? parseInt(req.query.id.toString()) : NaN
+
+    if (isNaN(id)) {
+      res.send('Invalid ID')
+      return
+    }
+
+    await App.db.models.WormsBotDraft.destroy({
+      where: { id, UserId: req.user.id },
+    })
+
+    res.redirect('/worms/drafts')
+  })
+
+  App.express.get('/worms/drafts/edit', async (req, res) => {
+    if (!req.user) {
+      res.redirect('/')
+      return
+    }
+
+    const id = req.query.id ? parseInt(req.query.id.toString()) : NaN
+
+    if (isNaN(id)) {
+      res.send('Invalid ID')
+      return
+    }
+
+    const bot = await App.db.models.WormsBotDraft.findOne({
+      where: { id, UserId: req.user.id },
+    })
+
+    if (!bot) {
+      res.send('Bot not found')
+      return
+    }
+
+    renderPage(App, req, res, {
+      page: 'worms-drafts-edit',
+      heading: 'Bot bearbeiten',
+      backButton: false,
+      content: `
+        <p>Programmiere deinen Bot mit JavaScript (unterstützt wird ES2023)</p>
+
+        <p>[<a href="/worms/drafts">schließen</a>]</p>
+
+        <div id="container" style="height: 800px"></div>
+
+        <link
+          rel="stylesheet"
+          data-name="vs/editor/editor.main"
+          href="/monaco/vs/editor.main.css"
+        />
+        <script>
+          var require = { paths: { vs: window.location.origin + '/monaco/vs' } };
+        </script>
+        <script src="/monaco/vs/loader.js"></script>
+        <script src="/monaco/vs/editor/editor.main.js"></script>
+
+        <script>
+          const myEditor = monaco.editor.create(document.getElementById("container"), {
+            value: \`${bot.code.replace(/`/, '\\`')}\`,
+            language: "typescript",
+            automaticLayout: true,
+            minimap: { enabled: false },
+            theme: 'vs-dark',
+            scrollBeyondLastLine: false,
+            padding: {
+              top: 10
+            },
+            scrollbar: {
+              alwaysConsumeMouseWheel: false
+            }
+          });
+
+          monaco.languages.typescript.typescriptDefaults.setCompilerOptions({
+            allowNonTsExtensions: true,
+            target: 99,
+            allowJs: true,
+            checkJs: true,
+          })
+        </script>
+      `,
+    })
   })
 }

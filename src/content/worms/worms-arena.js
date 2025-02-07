7ccd3cbe51f38e0529839d1aaff2ee9b5ea8ad10
Entkenntnis <Entkenntnis@users.noreply.github.com>
2025-02-07 14:28:42 +0100

    add 10 latest matches to arena

--- src/content/worms/worms-arena.js
+++ src/content/worms/worms-arena.js
@@ -499,6 +499,40 @@ export function setupWormsArena(App) {
           updateBotIdAndUpdateUI(parseInt(document.querySelector('select[name="bot"]').value))
         </script>
 
+        <h4 style="margin-top: 56px;">Letzte Matches</h4>
+        <table class="table">
+          <thead>
+            <tr>
+              <th>Bot Rot</th>
+              <th>Bot Grün</th>
+              <th>Ergebnis</th>
+              <th>Datum</th>
+            </tr>
+          </thead>
+          <tbody>
+            ${matches
+              .slice(0, 10)
+              .map(
+                (match) => `
+              <tr>
+                <td>${escapeHTML(
+                  botData.find((b) => b.id == match.redBotId)?.name ??
+                    '[gelöschter Bot]'
+                )}${match.status == 'red-win' ? ' 🏆' : ''}</td>
+                <td>${escapeHTML(
+                  botData.find((b) => b.id == match.greenBotId)?.name ??
+                    '[gelöschter Bot]'
+                )}${match.status == 'green-win' ? ' 🏆' : ''}</td>
+                <td>${match.status == 'red-win' ? 'Rot' : 'Grün'} gewinnt [<a href="/worms/arena/replay?id=${match.id}">ansehen</a>]</td>
+                <td>${App.moment(match.createdAt).locale('de').fromNow()}</td>
+              </tr>
+            `
+              )
+              .join('')}
+          </tbody>
+        </table>
+
+
         <div style="height: 200px;"></div>
       `,
       })
@@ -1007,7 +1041,7 @@ export function setupWormsArena(App) {
             const chart = new Chart(ctx, {
               type: 'line',
               data: {
-                labels: [${elos.map((elo, index) => `""`).join(',')}],
+                labels: [${elos.map(() => `""`).join(',')}],
                 datasets: [{
                   label: 'ELO',
                   data: [${elos.join(',')}],

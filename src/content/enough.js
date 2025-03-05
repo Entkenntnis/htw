668f20fac53310bed6888ef5327b5a21b85e924b
Entkenntnis <Entkenntnis@users.noreply.github.com>
2025-03-05 22:47:01 +0100

    fix text

--- src/content/enough.js
+++ src/content/enough.js
@@ -21,7 +21,7 @@ export function setupEnough(App) {
       content: `
         <p><a href="/map">zurück</a></p>
 
-        <p style="margin-bottom:48px; margin-top:36px; max-width: 65ch; ">Du gehst in die Bibliothek von Hack The Web. Du nimmst ein Buch aus dem Regal und beginnst zu lesen.</p>
+        <p style="margin-bottom:48px; margin-top:36px; max-width: 65ch;">Du gehst in die Bibliothek. Dort nimmst du ein Buch aus dem Regal und beginnst zu lesen.</p>
 
         <script>
 

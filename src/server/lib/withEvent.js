b9add902a7dfcb271ab8ccdd19517c4e91dc774f
Entkenntnis <Entkenntnis@users.noreply.github.com>
2025-08-13 14:12:19 +0200

    add api route for submitting events

---
+++ src/server/lib/withEvent.js
@@ -0,0 +1,17 @@
+/**
+ * @param {import("../../data/types.js").App} App
+ */
+export function withEvent(App) {
+  App.event = {
+    create: async (/** @type {string} */ key, /** @type {number} */ userId) => {
+      try {
+        await App.db.models.Event.create({
+          key: key.slice(0, 1000),
+          userId,
+        })
+      } catch (e) {
+        // ignore errors here, not critical
+      }
+    },
+  }
+}
---
+++ src/server/routes/event.js
@@ -0,0 +1,24 @@
+/**
+ * @param {import("../../data/types.js").App} App
+ */
+export function setupEvent(App) {
+  App.express.get('/api/event', async (req, res) => {
+    if (!req.user) {
+      res.status(403).send('Forbidden')
+      return
+    }
+    const userId = req.user.id
+    const event = req.query.key
+    if (
+      !event ||
+      typeof event !== 'string' ||
+      event.length === 0 ||
+      event.length > 1000
+    ) {
+      res.status(400).send('Bad Request, missing or invalid "key" parameter')
+      return
+    }
+    await App.event.create(event, userId)
+    res.status(200).send('✅')
+  })
+}

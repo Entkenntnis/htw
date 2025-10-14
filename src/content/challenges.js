1ff84c1078bf35c45d5fac8680d4678b6722fb1a
Entkenntnis <Entkenntnis@users.noreply.github.com>
2025-10-14 17:05:56 +0200

    split community challenges into files < 100kb

--- src/content/challenges.js
+++ src/content/challenges.js
@@ -1,9 +1,11 @@
 import { challengesAfterPassage } from './challenges-after-passage.js'
 import { part1 } from './challenges-part1.js'
 import { communityChallenges } from './community.js'
+import { communityChallenges2 } from './community-2.js'
 
 export const htwChallenges = [
   ...part1,
   ...challengesAfterPassage,
   ...communityChallenges,
+  ...communityChallenges2,
 ]

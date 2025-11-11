977472f74a850533c763737dec25e4af7b30a2d2
Entkenntnis <Entkenntnis@users.noreply.github.com>
2025-11-11 21:58:32 +0100

    finish experiment and change content

---
+++ public/comlink/77.js
@@ -0,0 +1,22 @@
+const COM_TREE = null /*{
+  character: 'KIWI',
+  dev: true,
+  messages: {
+    start: {
+      text: 'Na, kommst du klar?',
+      next: 'entry',
+    },
+    entry: {
+      text: 'Ich helfe dir gerne weiter, wenn du Fragen hast.',
+      options: [
+        { label: '... was muss ich machen? ...', next: 'strategy' },
+        { label: '... konkrete frage ...', next: 'question' },
+        {
+          label: '... Erzähl mal was von dir ...',
+          next: 'backstory',
+        },
+      ],
+    },
+  },
+}
+*/

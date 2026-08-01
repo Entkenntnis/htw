bcbfdbe10b3d3d12e019e48158765abc4ee1f04b
Entkenntnis <Entkenntnis@users.noreply.github.com>
2026-08-02 00:18:52 +0200

    fix css logo

--- src/content/challenges-part1.js
+++ src/content/challenges-part1.js
@@ -2380,7 +2380,7 @@ export const part1 = [
     renderAfterSolveText: () => {
       return {
         de: '<p>Die Farbe wurde zum Gedenken an die verstorbene Tochter des CSS-Entwicklers Eric A. Meyer benannt und findet sich im neuen <a href="https://de.m.wikipedia.org/wiki/Datei:Official_CSS_Logo.svg" target="_blank">CSS-Logo</a>.</p>',
-        en: '<p>The color was named in memory of the late daughter of CSS developer Eric A. Meyer and is featured in the new <a href="https://en.wikipedia.org/wiki/File:CSS3_logo.svg" target="_blank">CSS logo</a>.</p>',
+        en: '<p>The color was named in memory of the late daughter of CSS developer Eric A. Meyer and is featured in the new <a href="https://commons.wikimedia.org/wiki/File:Official_CSS_Logo.svg" target="_blank">CSS logo</a>.</p>',
       }
     },
   },
